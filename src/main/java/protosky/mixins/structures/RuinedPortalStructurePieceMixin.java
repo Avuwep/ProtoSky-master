ea959ccfdfa5541498c0470960a7b386388583f9
Avuwep <enderfender9@gmail.com>
2023-04-16 20:25:20 -0400

    Ruined Portals will no longer generate naturally.

---
+++ src/main/java/protosky/mixins/structures/RuinedPortalStructurePieceMixin.java
@@ -0,0 +1,25 @@
+package protosky.mixins.structures;
+
+import net.minecraft.structure.RuinedPortalStructurePiece;
+import net.minecraft.util.math.BlockBox;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.StructureWorldAccess;
+import net.minecraft.world.gen.StructureAccessor;
+import net.minecraft.world.gen.chunk.ChunkGenerator;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(RuinedPortalStructurePiece.class)
+public class RuinedPortalStructurePieceMixin {
+
+    //Prevent ruined portals from generating
+    @Inject(method = "generate", at = @At("HEAD"), cancellable = true)
+    private void onGenerate(StructureWorldAccess world, StructureAccessor structureAccessor, ChunkGenerator chunkGenerator, Random random, BlockBox chunkBox, ChunkPos chunkPos, BlockPos pivot, CallbackInfo ci) {
+        System.out.println("Portals blocked!");
+        ci.cancel();
+    }
+}
