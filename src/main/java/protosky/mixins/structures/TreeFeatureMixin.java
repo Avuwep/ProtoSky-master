3eb11a792ac18efd8b6437108602c68e321983ba
Avuwep <enderfender9@gmail.com>
2023-04-27 20:54:44 -0400

    More work on trees

---
+++ src/main/java/protosky/mixins/structures/TreeFeatureMixin.java
@@ -0,0 +1,26 @@
+package protosky.mixins.structures;
+
+
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.StructureWorldAccess;
+import net.minecraft.world.gen.feature.TreeFeature;
+import net.minecraft.world.gen.feature.TreeFeatureConfig;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.function.BiConsumer;
+
+@Mixin(TreeFeature.class)
+public class TreeFeatureMixin {
+    @Inject(method = "generate", at = @At("HEAD"), cancellable = true)
+    private void onGenerate(StructureWorldAccess world, Random random, BlockPos pos, BiConsumer<BlockPos, BlockState> rootPlacerReplacer, BiConsumer<BlockPos, BlockState> trunkPlacerReplacer, BiConsumer<BlockPos, BlockState> foliagePlacerReplacer, TreeFeatureConfig config, CallbackInfoReturnable ci) {
+        System.out.println("Trees blocked!");
+        ci.cancel();
+    }
+
+}
