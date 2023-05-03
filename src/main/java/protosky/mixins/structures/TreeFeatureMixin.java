933225338aa66c319b3aee65a8d14b1d31194ca8
Avuwep <enderfender9@gmail.com>
2023-05-02 20:51:03 -0400

    Updated to 1.19.4

--- src/main/java/protosky/mixins/structures/TreeFeatureMixin.java
+++ src/main/java/protosky/mixins/structures/TreeFeatureMixin.java
@@ -8,6 +8,7 @@ import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.gen.feature.TreeFeature;
 import net.minecraft.world.gen.feature.TreeFeatureConfig;
+import net.minecraft.world.gen.foliage.FoliagePlacer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,7 +20,7 @@ import java.util.function.BiConsumer;
 @Mixin(TreeFeature.class)
 public class TreeFeatureMixin {
     @Inject(method = "generate", at = @At("HEAD"), cancellable = true)
-    private void onGenerate(StructureWorldAccess world, Random random, BlockPos pos, BiConsumer<BlockPos, BlockState> rootPlacerReplacer, BiConsumer<BlockPos, BlockState> trunkPlacerReplacer, BiConsumer<BlockPos, BlockState> foliagePlacerReplacer, TreeFeatureConfig config, CallbackInfoReturnable ci) {
+    private void onGenerate(StructureWorldAccess world, Random random, BlockPos pos, BiConsumer<BlockPos, BlockState> rootPlacerReplacer, BiConsumer<BlockPos, BlockState> trunkPlacerReplacer, FoliagePlacer.BlockPlacer blockPlacer, TreeFeatureConfig config, CallbackInfoReturnable ci) {
         Chunk chunk = world.getChunk(pos);
         //Only generate trees if the chunks have been generated (Don't generate trees naturally but saplings should grow)
         if (!chunk.isLightOn()) {
